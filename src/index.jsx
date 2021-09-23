115db0a227347585f00cbd9115b1d58bd0cc9568
maker0n <60572514+Maker0N@users.noreply.github.com>
2021-09-23 18:28:55 +0500

    Initial commit

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+// eslint-disable-next-line import/extensions
+import Home from './components/home'
+
+const target = document.getElementById('root')
+
+ReactDOM.render(<Home />, target)
